7297e63650082f999dea88932293a1d9f96a135f
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2021-09-15 01:25:00 -0300

    Update index.sh

--- Post_build/index.sh
+++ Post_build/index.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-set -x
-cd CryptImage/
+cd ${Workdir}/Post_build/CryptImage/
+pwd
 
 if ! [ -z "${OPENWRT_BIN_FACTORY}" ];then
-    set -e
-    echo "Signing Factory firmware, Path: ${OPENWRT_BIN_FACTORY}"
-    ./pack.sh -f "${OPENWRT_BIN_FACTORY}" -o "${OPENWRT_BIN_FACTORY}_signed.bin"
-    echo "Firmware Factory successfully signed"
+  echo "Signing Factory firmware, Path: ${OPENWRT_BIN_FACTORY}"
+  chmod a+x pack.sh
+  ./pack.sh -f "${OPENWRT_BIN_FACTORY}" -o "${OPENWRT_BIN_FACTORY}_signed.bin"
+  echo "Firmware Factory successfully signed"
 fi
 
 # End
